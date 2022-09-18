498c08d8f7c42a67378f623842e2017ecbd3b565
f2lpOliveira <devfullstack@disponivel.online>
2022-09-17 21:23:33 -0300

    Estruturação de pastas e arquivos

---
+++ index.js
@@ -0,0 +1,17 @@
+// config inicial
+const express = require('express')
+const app = express()
+
+// depois do db
+const mongoose = require('mongoose')
+
+const Person = require('./models/Person')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
